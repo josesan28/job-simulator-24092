c11d8d2b3ccf87f3fb942dc4258002f04ef249a6
josesan28 <san24092@uvg.edu.gt>
2026-03-19 20:41:46 -0600

    Integración del frontend

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const teamsRouter = require("./src/routes/teams");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/teams", teamsRouter);
